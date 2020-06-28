90949af7cf42786f3fcac04c35a39946f4a1306f
Andrii Mishtal <mishtal.andriy@gmail.com>
2020-06-28 16:40:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.396cc17e3f50d69467e0382807f0e1f0.js"
 );
 
 self.addEventListener('message', (event) => {
